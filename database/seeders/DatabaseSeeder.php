982e4f1a72a04339b20ec86387a17d86dbc4d46a
Natthanan Prasertsri <61123827@g.cmru.ac.th>
2021-02-13 15:22:56 +0700

    สร้างฐานข้อมูลuser

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       $user = new User();
+       $user->name ="Natthanan Prasertsri";
+       $user->username ="admin";
+       $user->email ="natthanan@gmail.com";
+       $user->password = Hash::make("1234");
+       $user->save();
+       // \App\Models\User::factory(10)->create();
     }
 }
