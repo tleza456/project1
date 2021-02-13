542bed80c7f7459aec473d4f063719fc38dc7081
Natthanan Prasertsri <61123827@g.cmru.ac.th>
2021-02-13 13:30:31 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,17 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'] );
+    //return view('welcome');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [HomeController::class, 'about'] );
+    //return view('about');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [HomeController::class, 'contact'] );
+    //return view('contact');
 
 Route::get('/สวัสดี', function () {
     return "สวัสดี";
@@ -31,4 +29,8 @@ Route::get('/สวัสดี', function () {
 
 Route::get('/hello/{name}', function ($name) {
     return "สวัสดี $name";
+});
+
+Route::get('/calculator/{num1}/{num2}', function ($num1,$num2) {
+    return "ผลบวกของเลข $num1 และ $num2 คือ" . ($num1 + $num2);
 });
